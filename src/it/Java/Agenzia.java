926e3d02194b485d65d86be5849520eafe008a92
RANA Riccardo <riccardo.rana@soprasteria.com>
2022-10-11 17:00:38 +0200

    secondTry

--- src/it/Java/Agenzia.java
+++ src/it/Java/Agenzia.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 import static it.Java.Vacation.Vacanza.durataVacanza;
 
 public class Agenzia {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate giorno = LocalDate.of(2022, 10, 10);
@@ -19,39 +19,47 @@ public class Agenzia {
         int inizioGiorno, inizioMese, inizioAnno, fineGiorno, fineMese, fineAnno;
         LocalDate dataInizio, dataFine;
         String destinazione;
-        Vacanza vacanza;
+
+
 
         //if (d2.isAfter(d1)){}
 
         System.out.println("Ciao siamo la tua travel agency, per che destinazione vuoi prenotare la tua vacanza?");
         destinazione = in.nextLine();
         System.out.println("Benissimo, per che giorno avevi intenzione di partire?");
-        inizioGiorno = Integer.parseInt(in.nextLine());
+        inizioGiorno = in.nextInt();
         System.out.println("Di quale mese?");
-        inizioMese = Integer.parseInt(in.nextLine());
+        inizioMese = in.nextInt();
         System.out.println("Di quale anno?");
-        inizioAnno = Integer.parseInt(in.nextLine());
+        inizioAnno = in.nextInt();
         dataInizio = LocalDate.of(inizioAnno, inizioMese, inizioGiorno);
+        System.out.println(dataInizio);
 
         System.out.println("Benissimo, per che giorno avevi intenzione di tornare?");
-        fineGiorno = Integer.parseInt(in.nextLine());
+        fineGiorno = in.nextInt();
         System.out.println("Di quale mese?");
-        fineMese = Integer.parseInt(in.nextLine());
+        fineMese = in.nextInt();
         System.out.println("Di quale anno?");
-        fineAnno = Integer.parseInt(in.nextLine());
+        fineAnno = in.nextInt();
         dataFine = LocalDate.of(fineAnno, fineMese, fineGiorno);
+        System.out.println(dataFine);
+
+        if (dataInizio.isAfter(dataFine))
+            System.out.println("La data d'inizio non può essere dopo quella della fine!");
+        else {
+
+            try {
+                Vacanza vacanza = new Vacanza(destinazione, dataInizio, dataFine);
+                System.out.println("Hai prenotato la tua vacanza!");
+                System.out.println("Data inizio:" + dataInizio.format(df)); //11/10/2022!!!
+                System.out.println("Data fine:" + dataFine.format(df));
+                System.out.println("Durata:" + durataVacanza(dataInizio, dataFine));
 
 
-        try {
-            vacanza = new Vacanza(destinazione, dataInizio, dataFine);
-            System.out.println("Data inzio:" + dataInizio.format(df)); //11/10/2022!!!
-            System.out.println("Data fine:" + dataFine.format(df));
-            System.out.println("Durata:" + durataVacanza(dataInizio, dataFine));
+            } catch (Exception e) {
+                System.out.println("Non valido");
+            }
             in.close();
-
-        } catch (Exception e) {
-            System.out.println("Le date inserite non sono valide!");
         }
-
     }
 }
--- src/it/Java/Vacation/Vacanza.java
+++ src/it/Java/Vacation/Vacanza.java
@@ -1,5 +1,6 @@
 package it.Java.Vacation;
 
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
@@ -12,19 +13,17 @@ public class Vacanza {
     LocalDate now = LocalDate.now();
 
     public Vacanza(String destinazione, LocalDate dataInizio, LocalDate dataFine) throws Exception {
-        if (!destinazione.equals(" "))
-            this.destinazione = destinazione;
-        else
-            throw new Exception("Destinazione non valida!");
-        if (ChronoUnit.DAYS.between(this.dataInizio, now) < 0)
-            this.dataInizio = dataInizio;
-        else
-            throw new Exception("Data non valida!");
 
-        if (durataVacanza(this.dataInizio, this.dataFine) < 0)
-            this.dataFine = dataFine;
-        else
-            throw new Exception("La data della fine non può essere prima di quella dell'inizio!");
+        this.destinazione = destinazione;
+
+        if (this.dataInizio.isBefore(now)) {
+            this.dataInizio = dataInizio;
+        } else {
+            throw new Exception("Data non valida");
+        }
+
+        this.dataFine = dataFine;
+
     }
 
 
